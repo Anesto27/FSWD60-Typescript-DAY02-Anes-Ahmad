786a4297d8579959c586fb4ffe582f5402b779f6
Anesto27 <anes.2503@gmx.at>
2019-02-13 10:15:24 +0100

    basic2 insert in basic 1 finish

--- myscript.js
+++ myscript.js
@@ -1,3 +1,16 @@
+var __extends = (this && this.__extends) || (function () {
+    var extendStatics = function (d, b) {
+        extendStatics = Object.setPrototypeOf ||
+            ({ __proto__: [] } instanceof Array && function (d, b) { d.__proto__ = b; }) ||
+            function (d, b) { for (var p in b) if (b.hasOwnProperty(p)) d[p] = b[p]; };
+        return extendStatics(d, b);
+    };
+    return function (d, b) {
+        extendStatics(d, b);
+        function __() { this.constructor = d; }
+        d.prototype = b === null ? Object.create(b) : (__.prototype = b.prototype, new __());
+    };
+})();
 var Person = /** @class */ (function () {
     function Person(name, age, jobTitle) {
         this.name = "";
@@ -14,3 +27,20 @@ var Person = /** @class */ (function () {
 }());
 var test = new Person("Anton", "27", "Developer");
 document.write(test.sentence());
+var Student = /** @class */ (function (_super) {
+    __extends(Student, _super);
+    function Student(name, age, jobTitle, a, b) {
+        var _this = _super.call(this, name, age, jobTitle) || this;
+        _this.salary = "";
+        _this.jobLocation = "";
+        _this.salary = a;
+        _this.jobLocation = b;
+        return _this;
+    }
+    Student.prototype.sentence2 = function () {
+        return "Hello there, My name is " + this.name + " and I am " + this.age + " years old, and I am a " + this.jobTitle + " and I get " + this.salary + " \n\t\tevery month, and I work in " + this.jobLocation;
+    };
+    return Student;
+}(Person));
+var test2 = new Student("Ahmad", "27", "Junior Developer", "2000$", "Vienna");
+document.write(test2.sentence2());
--- myscript.ts
+++ myscript.ts
@@ -17,3 +17,23 @@ class Person{
 var test = new Person("Anton","27","Developer");
 
 document.write(test.sentence());
+
+
+class Student extends Person{
+	salary="";
+	jobLocation="";
+
+	constructor(name,age,jobTitle,a,b){
+		super(name,age,jobTitle);
+		this.salary=a;
+		this.jobLocation=b;
+	}
+	sentence2(){
+		return `Hello there, My name is ${this.name} and I am ${this.age} years old, and I am a ${this.jobTitle} and I get ${this.salary} 
+		every month, and I work in ${this.jobLocation}`
+	}
+
+}
+
+ var test2=new Student("Ahmad","27","Junior Developer","2000$","Vienna");
+ document.write(test2.sentence2());
